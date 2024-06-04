379e8d63e6cae01d129a1ef021474c57b066bcc3
WangXu <makewongms@163.com>
2024-06-04 21:23:08 +0800

    Add files via upload

---
+++ tests/feifei-art.bat
@@ -0,0 +1,3 @@
+
+.\python_embeded\python.exe -s Fooocus\entry_with_update.py --preset feifei-art --language zh_cn
+pause
---
+++ tests/feifei-photo.bat
@@ -0,0 +1,3 @@
+
+.\python_embeded\python.exe -s Fooocus\entry_with_update.py --preset feifei-photo --language zh_cn --vae-in-fp32
+pause
